d14a5aec696adf30e63d29480c1d28602703ac96
Nemoos <mr.matteo.romano.02@gmail.com>
2022-06-08 13:32:23 +0200

    Aggiunte caption e testo

--- code/noise.py
+++ code/noise.py
@@ -2,13 +2,13 @@ from os import path
 import pandas as pd
 
 df = pd.read_csv(
-    path.join(path.dirname(__file__), "../data/measure/Corrente di buio t lampadina.tsv"),
+    path.join(path.dirname(__file__), "../data/measure/Corrente di buio t candela.tsv"),
     sep='\t',
     engine="python",
 )
 
 print(df.I.mean())
-print(df.I.std() / df.I.mean())
+print(df.I.std())
 print(
-    (df.I.max() - df.I.min()) / (df.I.max() + df.I.min())
+    (df.I.max() - df.I.min()) / 2
 )
